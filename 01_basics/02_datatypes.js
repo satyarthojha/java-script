469e3b0cfafe794880ffc90dceab41bbe7a639d3
satyarthojha <137029861+satyarthojha@users.noreply.github.com>
2025-04-10 13:21:31 +0530

    container config done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,4 +1,16 @@
 const userId= 123;
 const userName= "John Doe";
 var userBalance= 1000.50;
-console.table(userId, userName, userBalance);
+//console.table(userId, userName, userBalance);
+
+//number
+//bigint
+//string
+//boolean
+//null->standalone value
+//undefined
+//symbol->unique value
+//object->collection of key-value pairs
+console.log(typeof accountId);
+console.log(typeof undefined);
+console.log(typeof null);
