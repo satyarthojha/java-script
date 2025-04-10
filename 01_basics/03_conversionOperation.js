fe68570f22d0092eb2dee95211abb1ad1fce6e86
satyarthojha <137029861+satyarthojha@users.noreply.github.com>
2025-04-10 14:48:17 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score="33";
+console.log(typeof score);
+
+let valueNumber=Number(score);
+console.log(typeof valueNumber);
+
+console.log(valueNumber);
+
+let str="33abs";
+console.log(typeof str);
+
+let num=Number(str);
+console.log(typeof num);
+
+console.log(num);
+
+let res=true;
+console.log(typeof res);
+let resNum=Number(res);
+console.log(resNum)
+console.log(typeof resNum);
+
