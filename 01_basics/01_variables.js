80f737be4815a37920aa5f4ae8f3f26cac0ca688
satyarthojha <137029861+satyarthojha@users.noreply.github.com>
2025-04-10 13:32:30 +0530

    Fixed login bug

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,3 +2,4 @@ const accountId= 1234567890;
 const accountName= "John Doe";
 var accountBalance= 1000.50;
 console.log(accountId, accountName, accountBalance);
+
--- 01_variablees.js
+++
@@ -1,4 +0,0 @@
-const accountId= 1234567890;
-const accountName= "John Doe";
-var accountBalance= 1000.50;
-console.log(accountId, accountName, accountBalance);
